4e543187e86184bdb4ac537ae89c86be0dbf81cc
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-08-12 12:28:42 +0200

    Coordinates error handling

--- avaj/weather/Coordinates.java
+++ avaj/weather/Coordinates.java
@@ -6,16 +6,14 @@ public class Coordinates {
     private int latitude;
     private int height;
 
-    public Coordinates(int longitude, int latitude, int height) {
+    public Coordinates(int longitude, int latitude, int height) throws ArithmeticException {
         
-        if (longitude < 0)
-            longitude = 0;
-        if (latitude < 0)
-            latitude = 0;
-        if (height < 0)
-            height = 0;
+        if ((longitude < 0) || (latitude < 0) || (height < 0)) {
+			throw new ArithmeticException("Error: Coordinates cannot be negative for aircrafts");
+		}
+        
         if (height > 100)
-            height = 100;
+            throw new ArithmeticException("Error: Height cannot be greater than 100");
         
         this.longitude = longitude;
         this.latitude = latitude;
