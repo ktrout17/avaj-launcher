d2a043a0ba371300b3c98249be301d4cf1db5aa9
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-08-12 12:42:55 +0200

    weather provider

---
+++ avaj/weather/WeatherProvider.java
@@ -0,0 +1,26 @@
+package avaj.weather;
+
+public class WeatherProvider {
+    
+    private static WeatherProvider weatherProvider = new WeatherProvider();
+    private static String[] weather = {
+        "RAIN",
+        "FOG",
+        "SUN",
+        "SNOW"
+    };
+
+    private WeatherProvider() {}
+
+    public static WeatherProvider getProvider() {
+
+        return WeatherProvider.weatherProvider;
+    }
+
+    public String getCurrentWeather(Coordinates coordinates) {
+
+        int value = coordinates.getLongitude() + coordinates.getLatitude() + coordinates.getHeight();
+
+        return weather[value % 4];
+    }
+}
