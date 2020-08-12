be798209470da138f9b335910f45994414eef41a
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-08-12 11:56:12 +0200

    coordinates

---
+++ avaj/simulator/aircraft/Aircraft.java
@@ -0,0 +1,5 @@
+package avaj.simulator.aircraft;
+
+public class Aircraft {
+    
+}
---
+++ avaj/weather/Coordinates.java
@@ -0,0 +1,36 @@
+package avaj.weather;
+
+public class Coordinates {
+    
+    private int longitude;
+    private int latitude;
+    private int height;
+
+    public Coordinates(int longitude, int latitude, int height) {
+        
+        if (longitude < 0)
+            longitude = 0;
+        if (latitude < 0)
+            latitude = 0;
+        if (height < 0)
+            height = 0;
+        if (height > 100)
+            height = 100;
+        
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.height = height;
+    }
+
+    public int getLongitude() {
+        return this.longitude;
+    }
+
+    public int getLatitude() {
+        return this.latitude;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+}
