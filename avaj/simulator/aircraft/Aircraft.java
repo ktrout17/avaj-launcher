8c6f8c68d16be7791de051d801dadc4581d9f5d2
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-08-12 12:13:45 +0200

    aircraft added

--- avaj/simulator/aircraft/Aircraft.java
+++ avaj/simulator/aircraft/Aircraft.java
@@ -1,5 +1,23 @@
 package avaj.simulator.aircraft;
 
+import avaj.weather.Coordinates;
+
 public class Aircraft {
     
+    protected long id;
+    protected String name;
+    protected Coordinates coordinates;
+    private static long idCounter = 0;
+
+    protected Aircraft(String name, Coordinates coordinates) {
+
+        this.id = this.nextId();
+        this.name = name;
+        this.coordinates = coordinates;
+    }
+
+    private long nextId() {
+        return Aircraft.idCounter++;
+    }
+
 }
