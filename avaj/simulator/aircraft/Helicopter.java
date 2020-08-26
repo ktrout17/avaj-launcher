d9bad9c5ef4e718681c27af0f8366ed1f9ccf2d9
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-08-26 09:29:20 +0200

    helicopter configured

--- avaj/simulator/aircraft/Helicopter.java
+++ avaj/simulator/aircraft/Helicopter.java
@@ -2,14 +2,14 @@ package avaj.simulator.aircraft;
 
 import java.util.HashMap;
 
-import avaj.weather.Coordinates;
 import avaj.simulator.WeatherTower;
+import avaj.weather.Coordinates;
 
-public class Baloon extends Aircraft implements Flyable {
+public class Helicopter extends Aircraft implements Flyable {
     
     private WeatherTower tower;
 
-    Baloon(String name, Coordinates coordinates) {
+    Helicopter(String name, Coordinates coordinates) {
         super(name, coordinates);
     }
 
@@ -22,48 +22,48 @@ public class Baloon extends Aircraft implements Flyable {
             private static final long serialVersionUID = 1L;
 
             {
-            put("SUN", "It's so hot, I’m sweating in spots I didn’t know I had.");
-            put("RAIN", "Rain, rain, go away! Please come back another day!");
-            put("FOG", "So much fog, I can't see a thing! Should've invested in those fog lights..");
-            put("SNOW", "Oh no, not the snow! I not prepared for this.");
+            put("SUN", "It's so hot, we I had installed that AC.");
+            put("RAIN", "Ah rain my old friend, at least I fixed the wipers.");
+            put("FOG", "So much fog, I can't see a thing! Radar, don't give in now.");
+            put("SNOW", "Oh no, not the snow! I hope my rotor doesn't freeze.");
         }};
 
         if (weather.equals("SUN")) 
             this.coordinates = new Coordinates(
-                coordinates.getLongitude() + 2,
+                coordinates.getLongitude() + 10,
                 coordinates.getLatitude() + 0,
-                coordinates.getHeight() + 4
+                coordinates.getHeight() + 2
             );
         else if (weather.equals("RAIN"))
             this.coordinates = new Coordinates(
-                coordinates.getLongitude() + 0,
+                coordinates.getLongitude() + 5,
                 coordinates.getLatitude() + 0,
-                coordinates.getHeight() - 5
+                coordinates.getHeight() + 0
         );
         else if (weather.equals("FOG"))
             this.coordinates = new Coordinates(
-                coordinates.getLongitude() + 0,
+                coordinates.getLongitude() + 1,
                 coordinates.getLatitude() + 0,
-                coordinates.getHeight() - 3
+                coordinates.getHeight() + 0
         );
         else if (weather.equals("SNOW"))
             this.coordinates = new Coordinates(
                 coordinates.getLongitude() + 0,
                 coordinates.getLatitude() + 0,
-                coordinates.getHeight() - 15
+                coordinates.getHeight() - 12
         );
 
-        System.out.println("Baloon# " + this.name + "(" + this.id + "): " + message.get(weather));
+        System.out.println("Helicopter# " + this.name + "(" + this.id + "): " + message.get(weather));
         if (this.coordinates.getHeight() == 0) {
-            System.out.println("Baloon# " + this.name + "(" + this.id + "): landing.");
+            System.out.println("Helicopter# " + this.name + "(" + this.id + "): landing.");
             this.tower.unregister(this);
-            System.out.println("Tower says: Baloon# " + this.name + "(" + this.id + ") unregistered from weather tower.");
+            System.out.println("Tower says: Helicopter# " + this.name + "(" + this.id + ") unregistered from weather tower.");
         }
     }
 
     public void registerTower(WeatherTower weatherTower) {
         this.tower = weatherTower;
         this.tower.register(this);
-        System.out.println("Tower says: Baloon# " + this.name + "(" + this.id + ") registered to weather tower.");
+        System.out.println("Tower says: Helicopter# " + this.name + "(" + this.id + ") registered to weather tower.");
     }
 }
