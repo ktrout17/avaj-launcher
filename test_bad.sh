53a81c29e0486dee683a629cd1b4e02e28bbe2c9
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-08-31 11:59:45 +0200

    made scripts easier for testing

--- test_bad.sh
+++ test_bad.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/bash
 
+RED='\e[31m'
+GREEN='\e[32m'
+BLUE='\e[34m'
+RC='\033[0m' # Reset color
+
+echo -e "${BLUE}Script will return ${RC}${GREEN}OK!${RC}${BLUE} if there are Exceptions as these are all bad tests.\nPress enter to continue${RC}"
+read _
+
 for file in ./bad_test_scenarios/*.txt
 do
-	if java avaj.simulator.Simulator $file | grep -q "Error"; then
-		echo "avaj.Simulator.Simulator " $file
-		echo "OK!"
-	else
-		echo "avaj.Simulator.Simulator " $file
-		echo "INCORRECT! Press enter to continue."
+	if java avaj.simulator.Simulator $file; then
+		echo -e "${RED}avaj.Simulator.Simulator " $file
+		echo -e "KO!${RC} Press enter to continue."
 		read _
+	else
+		echo -e "${GREEN}avaj.Simulator.Simulator " $file
+		echo -e "OK!${RC}\n"
 	fi
 done
--- test_good.sh
+++ test_good.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/bash
 
+RED='\e[31m'
+GREEN='\e[32m'
+RC='\033[0m' # Reset color
+
 for file in ./good_test_scenarios/*.txt
 do
     if java avaj.simulator.Simulator $file && [ -e simulation.txt ]; then
-        echo "avaj.Simulator.Simulator " $file
-	    echo "OK!"
+        echo -e "${GREEN}avaj.Simulator.Simulator " $file
+		echo -e "OK!${RC}\n"
         rm -rf simulation.txt
     else
-        echo "avaj.Simulator.Simulator " $file
-        echo "INCORRECT! Press enter to continue."
-        read _
+        echo -e "${RED}avaj.Simulator.Simulator " $file
+		echo -e "KO!${RC} Press enter to continue."
+		read _
     fi
 done
